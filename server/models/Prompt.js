be0a9743db59751a2500418e0d68928ff24b466c
khaleed opeloyeru <kopeloye@my.centennialcollege.ca>
2025-02-22 13:36:03 -0500

    final

--- server/models/Prompt.js
+++ server/models/Prompt.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PromptSchema = new mongoose.Schema({
-    chatId: { type: String, required: true, unique: true },
-    prompt: { type: String, required: true },
-    response: { type: String, required: true },
-    createdAt: { type: String, required: true },
-    chatTitle: { type: String, required: true },
-    upVotes: { type: Number, default: 0 },
-    downVotes: { type: Number, default: 0 }
+  chatId: { type: String, required: true, unique: true },
+  prompt: { type: String, required: true },
+  response: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  chatTitle: { type: String, required: true },
+  upVotes: { type: Number, default: 0 },
+  downVotes: { type: Number, default: 0 },
 });
 
-module.exports = mongoose.model('Prompt', PromptSchema);
+module.exports = mongoose.model("Prompt", PromptSchema);
